228a4b055120db2c3c622aa46496be891a1f31ce
Syngmaster <max6361@mail.ru>
2017-06-08 15:48:45 +0100

    Firebase set up

---
+++ SocialChatApp/SMSignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SMSignInViewController.swift
+//  SocialChatApp
+//
+//  Created by Syngmaster on 08/06/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+import UIKit
+
+class SMSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
